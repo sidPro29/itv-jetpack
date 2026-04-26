12a56d881b2d0bf5201a30616d10eda0beeeb20b
siddhartha verma <siddhartha.verma@futwork.com>
2026-04-26 13:07:08 +0530

    version upgrade before deployment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.notifiy.itv"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     buildTypes {
